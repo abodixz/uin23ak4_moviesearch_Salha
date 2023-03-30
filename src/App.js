a0857deca1c77541c49ed8a4c8d19f67e3c25ef5
abodixz <122395857+abodixz@users.noreply.github.com>
2023-03-30 09:33:04 +0200

    Routing updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+//import './style.css';
+import Search from './components/Search';
+import MovieInfo from './components/MovieInfo';
+import { Route, Routes } from 'react-router-dom';
 import './css/main.css';
+
+
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Routes>
+        <Route index element={<Search />} />
+        <Route path='/movie-info/:id' element={<MovieInfo/>} />
+    </Routes>
   );
 }
+
 export default App;
